29ad9a407ae67e3d662c30eac36ab48ab355a36e
Thy <nguyen.thy2986@gmail.com>
2022-08-03 11:22:05 -0400

    “version_1”

---
+++ tokenizing.h
@@ -0,0 +1,8 @@
+#ifndef _TOKENIZING_H_  // Prevent multiple inclusion of same header file
+#define _TOKENIZING_H_  
+#include <stdio.h>
+#include <string.h>		// To define functions for manipulating arrays of characters
+void tokenizing (void); 
+#endif
+
+
